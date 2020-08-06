d1f682a6f7f0b86e8ff7d0a909ff357840db062b
Marc Risney <marc.risney@gmail.com>
2020-08-05 22:56:41 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/johanbrandhorst/grpc-gateway-boilerplate
+
+go 1.12
+
+require (
+	github.com/gofrs/uuid v3.2.0+incompatible
+	github.com/golang/protobuf v1.4.2
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.0-beta.3
+	github.com/rakyll/statik v0.1.7
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/genproto v0.0.0-20200521103424-e9a78aa275b7
+	google.golang.org/grpc v1.29.1
+	google.golang.org/protobuf v1.23.0
+)
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"io/ioutil"
+	"mime"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/rakyll/statik/fs"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
+
+	"github.com/johanbrandhorst/grpc-gateway-boilerplate/gateway"
+	"github.com/johanbrandhorst/grpc-gateway-boilerplate/insecure"
+	pbExample "github.com/johanbrandhorst/grpc-gateway-boilerplate/proto"
+	"github.com/johanbrandhorst/grpc-gateway-boilerplate/server"
+
+	// Static files
+	_ "github.com/johanbrandhorst/grpc-gateway-boilerplate/statik"
+)
+
+// getOpenAPIHandler serves an OpenAPI UI.
+// Adapted from https://github.com/philips/grpc-gateway-example/blob/a269bcb5931ca92be0ceae6130ac27ae89582ecc/cmd/serve.go#L63
+func getOpenAPIHandler() http.Handler {
+	mime.AddExtensionType(".svg", "image/svg+xml")
+
+	statikFS, err := fs.New()
+	if err != nil {
+		panic("creating OpenAPI filesystem: " + err.Error())
+	}
+
+	return http.FileServer(statikFS)
+}
+
+func main() {
+	// Adds gRPC internal logs. This is quite verbose, so adjust as desired!
+	log := grpclog.NewLoggerV2(os.Stdout, ioutil.Discard, ioutil.Discard)
+	grpclog.SetLoggerV2(log)
+
+	addr := "0.0.0.0:10000"
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	s := grpc.NewServer(
+		// TODO: Replace with your own certificate!
+		grpc.Creds(credentials.NewServerTLSFromCert(&insecure.Cert)),
+	)
+	pbExample.RegisterUserServiceServer(s, server.New())
+
+	// Serve gRPC Server
+	log.Info("Serving gRPC on https://", addr)
+	go func() {
+		log.Fatal(s.Serve(lis))
+	}()
+
+	err = gateway.Run("dns:///" + addr)
+	log.Fatalln(err)
+}

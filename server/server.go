604a954c509b69af37c16e85c7ffa01a6071cf19
Marc Risney <marc.risney@gmail.com>
2020-08-05 23:14:44 -0700

    Update server.go

--- server/server.go
+++ server/server.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/gofrs/uuid"
 
-	pbExample "github.com/johanbrandhorst/grpc-gateway-boilerplate/proto"
+	pbExample "github.com/mrisney/grpc-gateway-boilerplate/proto"
 )
 
 // Backend implements the protobuf interface
